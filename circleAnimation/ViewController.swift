bad645ab60d7c9b5015bf82bcfbf885b6d81ac96
yavuzaytekin <yavuzaytekin219@gmail.com>
2020-05-16 13:01:00 +0300

    Initial Commit

---
+++ circleAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  circleAnimation
+//
+//  Created by Yavuz on 16.05.2020.
+//  Copyright © 2020 Yavuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
