80f933c69ebfd3e135903850652743970a286ade
yavuzaytekin <yavuzaytekin219@gmail.com>
2020-05-16 13:37:54 +0300

    -added basic animated circle

--- circleAnimation/ViewController.swift
+++ circleAnimation/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let shapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.lineCap = .round
+        shapeLayer.fillColor = UIColor.black.cgColor
+        shapeLayer.strokeEnd = 0
+        
+        view.layer.addSublayer(shapeLayer)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
-
-
+    
+    @objc func handleTap() {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        animation.toValue = 1
+        animation.duration = 2
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(animation, forKey: "basic")
+    }
 }
 
